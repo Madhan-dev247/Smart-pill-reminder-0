a5d1ef1c4d0183f9ad59640fe68be645b4259e68
Madhan-dev247 <madhanprasadm0@gmail.com>
2025-10-25 17:37:46 +0530

    Add a list of common medications

---
+++ medicinelist.js
@@ -0,0 +1,7 @@
+export const medicineList = [
+  "Aspirin", "Paracetamol", "Ibuprofen", "Metformin", "Amoxicillin",
+  "Omeprazole", "Atorvastatin", "Azithromycin", "Cetirizine", "Lisinopril",
+  "Simvastatin", "Metoprolol", "Losartan", "Levothyroxine", "Albuterol",
+  "Gabapentin", "Hydrochlorothiazide", "Prednisone", "Ciprofloxacin",
+  "Clopidogrel", "Warfarin", "Glipizide"
+];
